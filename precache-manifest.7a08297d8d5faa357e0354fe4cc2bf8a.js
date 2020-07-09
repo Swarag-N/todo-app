2f5c7f5ded7d23e36dee2a6b72aa20aa815396b0
whois-this <46181010+whois-this@users.noreply.github.com>
2020-07-09 23:52:24 +0530

    Updates

---
+++ precache-manifest.7a08297d8d5faa357e0354fe4cc2bf8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4183159f8cdac0acfa38922636ab23",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "5d8d339c8ff51fff45f6",
+    "url": "/todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b07411860c4ebff6e4fd",
+    "url": "/todo-app/static/js/2.2324f71d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-app/static/js/2.2324f71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8d339c8ff51fff45f6",
+    "url": "/todo-app/static/js/main.18626ea9.chunk.js"
+  },
+  {
+    "revision": "cb762c27a0e1e2a020b2",
+    "url": "/todo-app/static/js/runtime-main.bfcc1203.js"
+  }
+]);
